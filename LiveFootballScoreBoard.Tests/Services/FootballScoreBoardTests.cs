c946558bf492ea758193234ae30a9b468512c728
nataliia.novosad <nataliia.novosad@DESKTOP-R9ILUTD>
2024-03-21 22:05:54 +0200

    Test failure - preload data on scoreboard initialization with correctly serialized structure

--- LiveFootballScoreBoard.Tests/Services/FootballScoreBoardTests.cs
+++ LiveFootballScoreBoard.Tests/Services/FootballScoreBoardTests.cs
@@ -1,4 +1,6 @@
-﻿using LiveFootballScoreBoard.Models;
+﻿using System.Collections.Concurrent;
+using System.Text.Json;
+using LiveFootballScoreBoard.Models;
 using LiveFootballScoreBoard.Services;
 using Microsoft.Extensions.Logging;
 using Moq;
@@ -6,15 +8,22 @@ using Moq;
 namespace LiveFootballScoreBoard.Tests.Services
 {
 	[TestClass]
-	public class FootballScoreBoardTests
+	public class FootballScoreBoardTests : BaseTests<FootballScoreBoardService>
 	{
 		private const string MatchesKey = Constants.FOOTBALL_MATCHES_KEY;
 		private const ushort MatchDuration = Constants.FOOTBALL_MATCH_DURATION_MINS;
 		private const ushort MatchDelay = Constants.FOOTBALL_MATCH_OVERDUE_MINS;
 
 		private readonly Mock<IStorageService<string?>> _storageServiceMock;
-		private readonly Mock<ILogger<FootballScoreBoardService>> _loggerMock;
 		private readonly IFootballScoreBoardService _service;
+
+		private readonly ConcurrentDictionary<int, FootballMatch> _footballMatches = new()
+		{
+			[1] = new() { AwayTeam = "Mexico", HomeTeam = "Canada", StartTime = DateTime.Now, Scores = new ( 0, 0 ) },
+			[2] = new() { AwayTeam = "Germany", HomeTeam = "France", StartTime = DateTime.Now.AddHours(-1), Scores = new (3, 2) },	
+		};
+
+		private readonly string _nonDeserializableCacheValue = "{}";
 
 		public FootballScoreBoardTests()
 		{
@@ -40,6 +49,34 @@ namespace LiveFootballScoreBoard.Tests.Services
 		[TestMethod]
 		[ExpectedException(typeof(ArgumentNullException))]
 		public void Constructor_ThrowsExceptionWhenLoggerIsNull()
+		{
+			// Arrange
+			var emptyLogger = default(ILogger<FootballScoreBoardService>);
+
+			// Act
+			var service = new FootballScoreBoardService(_storageServiceMock.Object, emptyLogger);
+
+			// Assert
+		}
+
+		[TestMethod]
+		public void Constructor_PreloadsMatchesOnInitialization()
+		{
+			// Arrange
+			var serializedValue = JsonSerializer.Serialize(_footballMatches);
+			_storageServiceMock.Setup(t => t.GetItem(Constants.FOOTBALL_MATCHES_KEY))
+				.Returns(serializedValue);
+
+			// Act
+			var service = new FootballScoreBoardService(_storageServiceMock.Object, _loggerMock.Object);
+
+			// Assert
+			_storageServiceMock.Verify(t => t.GetItem(Constants.FOOTBALL_MATCHES_KEY), Times.Once);
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void StartFootballMatch_ThrowsExceptionWhenLoggerIsNull()
 		{
 			// Arrange
 			var emptyLogger = default(ILogger<FootballScoreBoardService>);
